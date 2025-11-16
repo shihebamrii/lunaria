7be1124295c6f9cc8ffdeadfac2122b681f1fc18
shihebaamrii <chihebamri699@gmail.com>
2025-11-16 10:05:32 +0100

    fixing typscript issue related to the env

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,9 +1,5 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_TECH_PASS: string
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv
+  readonly VITE_TECH_PASS?: string
 }
